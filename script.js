fccf2187770cd64465d525ad78589510107cffd3
Hzuchello <115723775+Hzuchello@users.noreply.github.com>
2025-01-19 23:18:34 -0300

    Primeiro Envio (GUI)

---
+++ script.js
@@ -0,0 +1,20 @@
+let myAge = 38;
+const myName = 'Henrique Zuchello';
+let isMarried = false;
+let info = [myAge, myName, isMarried];
+console.log(info);
+
+let apples = 4;
+let pears = 3;
+let salad = apples + pears;
+console.log('Salada: ', salad);
+
+const myContry = 'Brasil';
+const myCity = 'Curitiba';
+console.log(myCity,', ', myContry);
+
+//before 2023
+isMarried = false;
+// after july 2023
+isMarried = true;
+console.log(isMarried);
